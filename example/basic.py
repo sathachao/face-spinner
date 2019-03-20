02544e0e831a6a8ccd6de12f0f622a30802752e1
Satha Chaojaroenrat <sathachao@gmail.com>
2019-03-20 09:57:18 +0700

    Migrate example code

--- example/basic.py
+++ example/basic.py
@@ -1,9 +1,9 @@
 
 import os
+import sys
 import numpy as np
 import torch
 import cv2
-import torch
 
 from face_spinner.utils import Window, draw_face, rotate_bound
 from face_spinner.models import PCN1, PCN2, PCN3
@@ -11,7 +11,13 @@ from face_spinner.facade import PCN
 
 
 if __name__ == '__main__':
-    
+
+    if len(sys.argv) < 2:
+        print("Insufficient input. Please input filename after the command.")
+        exit(1)
+
+    img_path = sys.argv[1]
+
     pcn1_state_dict = torch.load("saved_models/model_0.sdict")
     pcn1 = PCN1()
     pcn1.load_state_dict(pcn1_state_dict)
@@ -26,14 +32,17 @@ if __name__ == '__main__':
 
     model = PCN(pcn1, pcn2, pcn3)
 
-    img = cv2.imread("face-tilt.jpg")
+    img = cv2.imread(img_path)
     faces = model.detect(img)
 
     if len(faces) == 0:
         print("No face detected. Try again")
+        exit(1)
     elif len(faces) > 1:
         print("Too many faces detected. Try again")
+        exit(1)
     else:
         face = faces[0]
         rotated_img = rotate_bound(img, face.angle)
+        print("Complete successfully. Writing to output.jpg")
         cv2.imwrite("output.jpg", rotated_img)
